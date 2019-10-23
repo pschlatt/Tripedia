b00e45ad821672633be2b1b6a2a601abfa7720db
davidagitlen <davidagitlen@gmail.com>
2019-10-22 18:34:44 -0600

    Change connection function to addressData

--- spec/services/geocode_service_spec.rb
+++ spec/services/geocode_service_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe GeocodeService do
   describe 'instance methods' do
-    describe '#coordinates(location)' do
-      it 'returns the coordinates for a location requested' do
+    describe '#addressData(location)' do
+      it 'returns the addressData for a location requested' do
         geo = GeocodeService.new
         location = "denver co"
-        response = geo.coordinates(location)
+        response = geo.addressData(location)
         expect(response).to be_a(Hash)
       end
     end
