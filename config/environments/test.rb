c38ed730aac5dbb2c09767246b82e9c790aae6eb
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-10-24 12:46:35 -0600

    Changes to test Env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
+  
+  config.active_record.maintain_test_schema = true
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
