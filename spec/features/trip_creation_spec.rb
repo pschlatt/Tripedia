df200e34136a041734a9cda2adb4d0c18febabe4
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-26 10:51:34 -0600

    Rename route creation spec to trip creation

---
+++ spec/features/trip_creation_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Route_Creation', type: :request do
+  it "can take origin and destination of a trip and return geocoded points" do
+    user = User.create!(email: "yo@bach.com", password: "password")
+
+    post "/api/v1/users/#{user.id}/trips?origin=denver,co&destination=taos,nm"
+    expect(response).to be_successful
+
+    trip = JSON.parse(response.body)
+
+    expect(trip).to have_key("id")
+    expect(trip).to have_key("origin")
+    expect(trip).to have_key("destination")
+    expect(trip).to have_key("user_id")
+    expect(trip).to have_key("duration")
+    expect(trip).to have_key("distance")
+    expect(trip).to have_key("user_id")
+  end
+end
