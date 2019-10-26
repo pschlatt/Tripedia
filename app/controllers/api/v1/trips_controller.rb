a729f93c68c0e8ac04f38c5c6f74690b3a5bc831
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-26 11:20:05 -0600

    Replace strings of origin and destination with lat and long for custom pins

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -3,12 +3,24 @@ class Api::V1::TripsController < ApplicationController
   def create
     facade = DirectionsMatrixFacade.new(trip_params)
     user = User.find(trip_params[:user_id])
+    origin = origin_coords
+    destination = destination_coords
     trip_data = facade.return_trip_details
-    origin = trip_data[:origin_addresses].join
-    destination = trip_data[:destination_addresses].join
     distance = trip_data[:rows][0][:elements][0][:distance][:text]
     duration = trip_data[:rows][0][:elements][0][:duration][:text]
     render json: Trip.create(origin: origin, destination: destination, distance: distance, duration: duration, user_id: user.id)
+  end
+
+  def origin_coords
+    location = trip_params['origin']
+    geo = GeocodeFacade.new(location)
+    geo.coordinates
+  end
+
+  def destination_coords
+    location = trip_params['destination']
+    geo = GeocodeFacade.new(location)
+    geo.coordinates
   end
 
   private
