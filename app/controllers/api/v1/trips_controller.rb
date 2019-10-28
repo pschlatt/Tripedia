e550688f6ffd13c33d1d0821b0f7bb4afa6a2cf6
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-27 21:34:13 -0600

    Add attractions method to trips controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -11,34 +11,11 @@ class Api::V1::TripsController < ApplicationController
     render json: Trip.create(origin: origin, destination: destination, distance: distance, duration: duration, user_id: user.id)
   end
 
-  def radii
+  def attractions
     trip = Trip.find(params[:id])
-    YelpFacade.get_attractions_on_route(trip)
-    # trip = Trip.find(params[:id])
-    # distance = trip[:distance].to_i
-    # str_or_coords = trip[:origin]
-    # str_dest_coords = trip[:destination]
-    # origin = eval(str_or_coords)
-    # destination = eval(str_dest_coords)
-    # origin_lat_and_lng = origin.values
-    # dest_lat_and_lng = destination.values
-    # interval_count = distance / 30
-    # x2 = dest_lat_and_lng[0]
-    # x1 = origin_lat_and_lng[0]
-    # y2 = dest_lat_and_lng[1]
-    # y1 = origin_lat_and_lng[1]
-    # lat_displacement = (x2 - x1)/interval_count
-    # lng_displacement = (y2 - y1)/interval_count
-    # array_of_stops = interval_count.times do |interval|
-    #   new_lat = x1 + lat_displacement
-    #   new_lng = y1 + lng_displacement
-    #   x1 = new_lat
-    #   y1 = new_lng
-    # end
-    # array_of_stops
+    categories = params[:categories]
+    render json: YelpFacade.get_attractions_on_route(trip, categories)
   end
-
-
 
   private
 
