83575f1911b49c35162bb2eae24c64c4b6b910ce
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-27 18:20:17 -0600

    Add loop to collect lat and long for each stop to then give to yelp service to return interests
    Co-authored-by: Chris <c.lane.dev@gmail.com>
    Co-authored-by: David <davidagitlen@gmail.com>
    Co-authored-by: Paul <pschlatt@gmail.com>

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -11,32 +11,31 @@ class Api::V1::TripsController < ApplicationController
     render json: Trip.create(origin: origin, destination: destination, distance: distance, duration: duration, user_id: user.id)
   end
 
-  # def stops
-  #   binding.pry
-  # end
-
   def radii
-    binding.pry
     trip = Trip.find(params[:id])
-    str_or_coords = trip[:origin]
-    str_dest_coords = trip[:destination]
-    origin = eval(str_or_coords)
-    destination = eval(str_dest_coords)
-    origin_lat_and_lng = origin.values
-    dest_lat_and_lng = destination.values
-    interval_distance = distance / 30
-    x2 = dest_lat_and_lng[0]
-    x1 = origin_lat_and_lng[0]
-    y2 = dest_lat_and_lng[1]
-    y1 = origin_lat_and_lng[1]
-    lat_displacement = (x2 - x1)/interval_distance
-    lng_displacement = (y2 - y1)/interval_distance
-    new_lat = x1 + lat_displacement
-    new_lng = y1 + lng_displacement
-    new_lat_and_lng << new_lat
-    new_lat_and_lng << new_lng
-    new_point_lat = new_lat_and_lng[0] + lat_displacement
-    new_point_lng = new_lat_and_lng[1] + lng_displacement
+    Yelp.get_attractions_on_route(trip)
+    # trip = Trip.find(params[:id])
+    # distance = trip[:distance].to_i
+    # str_or_coords = trip[:origin]
+    # str_dest_coords = trip[:destination]
+    # origin = eval(str_or_coords)
+    # destination = eval(str_dest_coords)
+    # origin_lat_and_lng = origin.values
+    # dest_lat_and_lng = destination.values
+    # interval_count = distance / 30
+    # x2 = dest_lat_and_lng[0]
+    # x1 = origin_lat_and_lng[0]
+    # y2 = dest_lat_and_lng[1]
+    # y1 = origin_lat_and_lng[1]
+    # lat_displacement = (x2 - x1)/interval_count
+    # lng_displacement = (y2 - y1)/interval_count
+    # array_of_stops = interval_count.times do |interval|
+    #   new_lat = x1 + lat_displacement
+    #   new_lng = y1 + lng_displacement
+    #   x1 = new_lat
+    #   y1 = new_lng
+    # end
+    # array_of_stops
   end
 
 
--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -9,10 +9,35 @@ class YelpFacade
     Attraction.new(grabber.interests(@category))
   end
 
+  def self.get_attractions_on_route(trip)
+    distance = trip[:distance].to_i
+    str_or_coords = trip[:origin]
+    str_dest_coords = trip[:destination]
+    origin = eval(str_or_coords)
+    destination = eval(str_dest_coords)
+    origin_lat_and_lng = origin.values
+    dest_lat_and_lng = destination.values
+    interval_count = distance / 30
+    x2 = dest_lat_and_lng[0]
+    x1 = origin_lat_and_lng[0]
+    y2 = dest_lat_and_lng[1]
+    y1 = origin_lat_and_lng[1]
+    lat_displacement = (x2 - x1)/interval_count
+    lng_displacement = (y2 - y1)/interval_count
+    array_of_stops = interval_count.times do |interval|
+      new_lat = x1 + lat_displacement
+      new_lng = y1 + lng_displacement
+
+      x1 = new_lat
+      y1 = new_lng
+    end
+    array_of_stops
+  end
+
   private
 
   def grabber
-    YelpService.new(@location)
+    @_grabber ||= YelpService.new(@location)
   end
 
 end
