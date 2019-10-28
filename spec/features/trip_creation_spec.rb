f3fa5f7027aa73e56fdd7b32e327b9c1dabbe884
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-27 22:46:32 -0600

    Remove other keys that were no longer to be returned in response

--- spec/features/trip_creation_spec.rb
+++ spec/features/trip_creation_spec.rb
@@ -8,12 +8,8 @@ RSpec.describe 'Trip_Creation', type: :request do
     expect(response).to be_successful
 
     trip = JSON.parse(response.body)
-    expect(trip).to have_key("id")
-    expect(trip).to have_key("origin")
-    expect(trip).to have_key("destination")
-    expect(trip).to have_key("user_id")
-    expect(trip).to have_key("duration")
-    expect(trip).to have_key("distance")
-    expect(trip).to have_key("user_id")
+
+    expect(trip).to have_key("trip")
+    expect(trip).to have_key("attractions")
   end
 end
