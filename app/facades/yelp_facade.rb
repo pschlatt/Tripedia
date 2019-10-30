72dbd2407a0650561874ca93312b7384aa4df3f6
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-29 21:23:50 -0600

    Add origin and destination lat and long to yelp radii

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -25,6 +25,8 @@ class YelpFacade
     lat_displacement = (x2 - x1)/interval_count
     lng_displacement = (y2 - y1)/interval_count
     yelp_radii = []
+    yelp_radii.push(origin_lat_and_lng)
+    yelp_radii.push(dest_lat_and_lng)
     interval_count.times do |interval|
       lat_and_lng = {}
       new_lat = x1 + lat_displacement
