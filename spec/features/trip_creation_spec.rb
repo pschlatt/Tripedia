6ab8193776a10483860476922c115321c1e0b895
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-10-30 18:26:28 -0600

    Add mock data for sea-mnn trip and comment out yelp facade being called

--- spec/features/trip_creation_spec.rb
+++ spec/features/trip_creation_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Trip_Creation', type: :request do
   it "can take origin and destination of a trip and return geocoded points" do
+    # response =
+    #   (user = User.create!(name: "miss buttersworth", email: "yo@bach.com", password: "password")
+    #   trip_created = File.open("./fixtures/trip_creation.json")
+    #   stub_request(:post, "/api/v1/users/#{user.id}/trips?origin=denver,co&destination=taos,nm").to_return(status:200, body:trip_created))
+    # trip = JSON.parse(response.response.body)
+    #
+    # expect(trip).to have_key("trip")
+    # expect(trip).to have_key("attractions")
+    user = User.create!(name: "miss buttersworth", email: "yo@bach.com", password: "password")
 
-    response =
-      (user = User.create!(name: "miss buttersworth", email: "yo@bach.com", password: "password")
-      trip_created = File.open("./fixtures/trip_creation.json")
-      stub_request(:post, "/api/v1/users/#{user.id}/trips?origin=denver,co&destination=taos,nm").to_return(status:200, body:trip_created))
-    trip = JSON.parse(response.response.body)
+    post "/api/v1/users/#{user.id}/trips?origin=seattle,wa&destination=minneapolis,mn"
+    binding.pry
+    trip_data = response.body
 
-    expect(trip).to have_key("trip")
-    expect(trip).to have_key("attractions")
+    File.open("sea_mnn_trip.json", "w") do |f|
+      f.write(JSON.parse(trip_data))
+    end
   end
 end
